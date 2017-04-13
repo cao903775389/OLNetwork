755a6484d9f12e8f5b78e9cb0ccb18199b8a5cfb
逢阳曹 <903775389@qq.com>
2017-04-13 18:18:55 +0800

    demo完善

---
+++ Example/OLNetwork/TestTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TestTableViewCell.swift
+//  OLNetwork
+//
+//  Created by 逢阳曹 on 2017/4/13.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class TestTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var coverImgView: UIImageView!
+    
+    @IBOutlet weak var title: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Example/OLNetwork/ViewController.swift
+++ Example/OLNetwork/ViewController.swift
@@ -11,21 +11,95 @@ import UIKit
 import OLNetwork
 import AFNetworking
 import SnapKit
+import YYModel
 
-class ViewController: UIViewController {
+private let identifier = "identifier"
 
-    //
-//    var
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, OLHttpRequestDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var request: BeautyMAPIHttpRequest!
+    
+    var data: [TrialListItemModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.estimatedRowHeight = 200
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.register(UINib(nibName: "TestTableViewCell", bundle: nil), forCellReuseIdentifier: identifier)
+        
         // Do any additional setup after loading the view, typically from a nib.
+        let param: [String: AnyObject] = ["rd": OLCode.OL_TrialList.rawValue as AnyObject, "ie": 1 as AnyObject]
+        
+        self.request = BeautyMAPIHttpRequest(delegate: self, requestMethod: OLHttpMethod.GET, requestUrl: MAPIURL.V130.rawValue, requestArgument: param, OL_CODE: OLCode.OL_TrialList)
+        
+        OLHttpRequestManager.sharedOLHttpRequestManager.sendHttpRequest(request: self.request)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: identifier)
+        if cell == nil {
+            cell = TestTableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: identifier)
+        }
+        let model = self.data[indexPath.row]
+        (cell as! TestTableViewCell).imageView?.setImageWith(URL(string: model.iu!)!)
+        (cell as! TestTableViewCell).title.text = model.tt
+        return cell!
+    }
+    
+    //HttpRequestDelegate
+    func ol_requestFinished(request: OLHttpRequest) {
+        
+        if let json = request.responseObject {
+            let response = json["data"] as? [String: AnyObject]
+            let trialData = response?["trylist"] as? [[NSObject: AnyObject]]
+            
+            if let trialJSON = trialData {
+                for trial in trialJSON {
+                    let trialModel = TrialListItemModel.yy_model(with: trial)
+                    self.data.append(trialModel!)
+                }
+            }
+            
+            
+            self.tableView.reloadData()
+        }
+    }
+    
+    func ol_requestFailed(request: OLHttpRequest) {
+        
+    }
 }
 
+
+class TrialListItemModel: NSObject {
+    /// 试用ID
+    var tryid:String?
+    /// 试用标题
+    var tt:String?
+    /// 试用数量
+    var tnum:String?
+    /// 试用人气
+    var wnum:String?
+    /// 开始时间
+    var stat:String?
+    /// 结束时间
+    var end:String?
+    /// 图片地址
+    var iu:String?
+    /// 使用介绍
+    var des:String?
+    /// 申请状态
+    var stu:String?
+    /// 试用规格 0正装 1小样 2中样
+    var type: String?
+    /// 试用状态
+    var tstu:String?
+}
